40fbcea10660dd7c8c512776ab8c6c3ca9346508
Agurato <Agurato@users.noreply.github.com>
2021-12-04 21:20:46 +0100

    Fix day 4 test

--- day4_test.go
+++ day4_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestDay4(t *testing.T) {
-	values, err := ReadLines("inputs/day4.txt")
+	values, err := ReadLinesSep("inputs/day4.txt", "\n\n")
 	if err != nil {
 		panic(err)
 	}
@@ -18,7 +18,7 @@ func TestDay4(t *testing.T) {
 var resultDay4 int
 
 func BenchmarkDay4Part1(b *testing.B) {
-	values, _ := ReadLines("inputs/day4.txt")
+	values, _ := ReadLinesSep("inputs/day4.txt", "\n\n")
 	var r int
 	for i := 0; i < b.N; i++ {
 		r = day4Part1(values)
@@ -27,7 +27,7 @@ func BenchmarkDay4Part1(b *testing.B) {
 }
 
 func BenchmarkDay4Part2(b *testing.B) {
-	values, _ := ReadLines("inputs/day4.txt")
+	values, _ := ReadLinesSep("inputs/day4.txt", "\n\n")
 	var r int
 	for i := 0; i < b.N; i++ {
 		r = day4Part2(values)
